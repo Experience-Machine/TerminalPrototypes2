23dd8ea79e5f4366a646c1125cac7e213376ae1c
Taran <TaranChristensen@gmail.com>
2016-04-30 14:59:56 -0700

    Basic Tile functionality
    Tiles appear/disappear as you mouse over them. Level1GameState has
    functionality for spawning the tiles in (in the form of a tupple grid).
    The Tile prefab is spawned in with a white color. The prefab can change
    pretty easily.

---
+++ Assets/Prototype1Scripts/TileBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileBehavior : MonoBehaviour
+{
+    private Renderer renderer;
+    // Use this for initialization
+    void Start () 
+    {
+        renderer = GetComponent<Renderer>();
+        renderer.enabled = false;
+    }
+       
+    // Update is called once per frame
+    void Update () 
+    {
+        
+    }
+
+    void OnMouseEnter()
+    {
+        renderer.enabled = true;
+        renderer.material.color = Color.white;
+    }
+    void OnMouseExit()
+    {
+        renderer.enabled = false;
+    }
+}
