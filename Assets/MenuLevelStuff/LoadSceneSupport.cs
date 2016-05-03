95b80056e1e5a0a880df137651a89ca7356b11be
Taran <TaranChristensen@gmail.com>
2016-05-02 20:56:12 -0700

    Main Menu: Proto6 button and text change

--- Assets/MenuLevelStuff/LoadSceneSupport.cs
+++ Assets/MenuLevelStuff/LoadSceneSupport.cs
@@ -12,6 +12,7 @@ public class LoadSceneSupport : MonoBehaviour {
     public Button mPrototype3Button;
     public Button mPrototype4Button;
     public Button mPrototype5Button;
+    public Button mPrototype6Button;
 
     // Use this for initialization
     void Start () {
@@ -22,13 +23,14 @@ public class LoadSceneSupport : MonoBehaviour {
         mPrototype3Button = GameObject.Find("Prototype3Button").GetComponent<Button>();
         mPrototype4Button = GameObject.Find("Prototype4Button").GetComponent<Button>();
         mPrototype5Button = GameObject.Find("Prototype5Button").GetComponent<Button>();
-
+        mPrototype6Button = GameObject.Find("Prototype6Button").GetComponent<Button>();
         
         mPrototype1Button.onClick.AddListener(Button1Service);
         mPrototype2Button.onClick.AddListener(Button2Service);
         mPrototype3Button.onClick.AddListener(Button3Service);
         mPrototype4Button.onClick.AddListener(Button4Service);
         mPrototype5Button.onClick.AddListener(Button5Service);
+        mPrototype6Button.onClick.AddListener(Button6Service);
     }
 
     #region Button service function
@@ -54,6 +56,11 @@ public class LoadSceneSupport : MonoBehaviour {
         private void Button5Service()
         {
             LoadScene("Prototype5");
+        }
+
+        private void Button6Service()
+        {
+            LoadScene("Prototype6");
         }
     #endregion
 
